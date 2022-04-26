86da65fe74421775542d47fa16991bba50c0e3e3
ChristinaLast <christina.last@outlook.com>
2022-04-26 21:57:31 +0100

    :card_file_box: updating the settings to include  and  needed for migrations to complete

--- lib_metadata_db/settings.py
+++ lib_metadata_db/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         "NAME": os.getenv("DB_NAME_NEWSPAPERS"),
         "USER": os.getenv("DB_USERNAME_NEWSPAPERS"),
         "PASSWORD": os.getenv("DB_PASSWORD_NEWSPAPERS"),
@@ -29,15 +29,45 @@ DATABASES = {
         "ATOMIC_REQUESTS": True,
     },
 }
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 
 INSTALLED_APPS = [
+    "django.contrib.admin",
     "django.contrib.contenttypes",
     "django.contrib.auth",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.gis",
     "newspapers.apps.NewspapersConfig",
+    "press_directories.apps.PressDirectoriesConfig",
+    "gazetteer.apps.GazetteerConfig",
 ]
 
 USE_TZ = False
