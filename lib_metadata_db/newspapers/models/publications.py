eb4e69e283f6e8daa63c40cdfc38db06ad22c1c3
ChristinaLast <christina.last@outlook.com>
2022-04-26 19:42:56 +0100

    :sparkles: adding foreign key relation to publications table for link to

--- lib_metadata_db/newspapers/models/publications.py
+++ lib_metadata_db/newspapers/models/publications.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 from .newspapers_model import NewspapersModel
+from .mitchells_publications import MitchellsPublication
 
 
 class Publication(NewspapersModel):
     publication_code = models.CharField(max_length=30, default="")
     title = models.CharField(max_length=255, default="")
     location = models.CharField(max_length=255, default="")
+    mitchells_publication = models.ForeignKey(
+        MitchellsPublication, on_delete=models.SET_NULL, verbose_name="mitchells", null=True
+    )
 
     class Meta:
         app_label = "newspapers"
