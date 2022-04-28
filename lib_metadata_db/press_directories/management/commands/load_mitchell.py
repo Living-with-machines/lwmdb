c8ee2b8f835956b3058ed51c37a71c11bce01569
Kaspar Beelen <kbeelen@turing.ac.uk>
2022-04-28 07:52:30 +0100

    add load mitchell

---
+++ lib_metadata_db/press_directories/management/commands/load_mitchell.py
@@ -0,0 +1,42 @@
+from lib_metadata_db.press_directories.models.mitchells import Mitchells
+from csv import DictReader
+from django.core.management import BaseCommand
+import pandas as pd
+
+# Import the model 
+
+
+ALREDY_LOADED_ERROR_MESSAGE = """
+If you need to reload the child data from the CSV file,
+first delete the db.sqlite3 file to destroy the database.
+Then, run `python manage.py migrate` for a new empty
+database with tables"""
+
+
+
+
+class Command(BaseCommand):
+    # Show this when the user types help
+    help = "Loads data from Mitchells press directories"
+
+    def handle(self, *args, **options):
+    
+        # Show this if the data already exist in the database
+        if Mitchells.objects.exists():
+            print('child data already loaded...exiting.')
+            print(ALREDY_LOADED_ERROR_MESSAGE)
+            return
+            
+        # Show this before loading the data into the database
+        print("Loading childrens data")
+
+        data = pd.read_csv('../data/mitchells.csv')
+        data.fillna('',inplace=True)
+        #Code to load the data into database
+        for i,row in data.iterrows():
+            record=Mitchells(title=row['title'], political_leaning_raw=row['political_leaning_raw'], price_raw=row['price_raw'],
+            				day_of_publication_raw=row['day_of_publication_raw'], date_established_raw=row['date_established_raw'], 
+            				place_of_circulation_raw=row['place_circulation_raw'], publication_district_raw=row['publication_district_raw'],
+            				publication_county_raw=row['publication_county_raw'], persons=row['persons'], organisations=row['organisations']
+            				)  
+            record.save()
