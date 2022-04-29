9ace344d749d8691d3911be7084ff2a3e0d061b6
ChristinaLast <christina.last@outlook.com>
2022-04-29 15:29:37 +0100

    :art: linting on script

--- lib_metadata_db/newspapers/management/commands/import_items.py
+++ lib_metadata_db/newspapers/management/commands/import_items.py
@@ -19,75 +19,79 @@ Then, run `python manage.py migrate` for a new empty
 database with tables"""
 
 # Set path to directory where your metadata blob (alto2txt-ed) is mounted.
-fullpath = input('Enter full path to to the directory where the metadata is stored: ')
-data_provider_name = input('Provide one name for data_provider: (lwm, hmd, bna, jisc): ')
-alltitles = glob('{}/*'.format(fullpath))
+fullpath = input("Enter full path to to the directory where the metadata is stored: ")
+data_provider_name = input(
+    "Provide one name for data_provider: (lwm, hmd, bna, jisc): "
+)
+alltitles = glob("{}/*".format(fullpath))
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if Item.objects.exists():
-            print('child data already loaded...exiting.')
+            print("items data already loaded...exiting.")
             print(ALREDY_LOADED_ERROR_MESSAGE)
             return
         # Show this before loading the data into the database
         print("Loading items data")
-        #Code to load the data into database
+        # Code to load the data into database
         for title in alltitles:
-            allyears = glob('{}/*'.format(title))
+            allyears = glob("{}/*".format(title))
             for year in allyears:
-                allissues = glob('{}/*'.format(year))
+                allissues = glob("{}/*".format(year))
                 for issue in allissues:
-                    metadataxmls = glob('{}/*.xml'.format(issue))
+                    metadataxmls = glob("{}/*.xml".format(issue))
                     for metadataxml in metadataxmls:
-                        with open(metadataxml, 'r') as meta:
-                            soup = BeautifulSoup(meta, 'lxml')
-                            pubmeta = soup.find('publication') #!
-                            processmeta = soup.find('process') #!
-                            issuemeta = pubmeta.find('issue') #!
-                            lwm_tool = processmeta.find('lwm_tool') #!
-                            itemmeta = issuemeta.find('item') #!
+                        with open(metadataxml, "r") as meta:
+                            soup = BeautifulSoup(meta, "lxml")
+                            pubmeta = soup.find("publication")
+                            processmeta = soup.find("process")
+                            issuemeta = pubmeta.find("issue")
+                            lwm_tool = processmeta.find("lwm_tool")
+                            itemmeta = issuemeta.find("item")
                             publication = Publication(
-                                publication_code = pubmeta.get('id'),
-                                title = pubmeta.find('title').text,
-                                location = pubmeta.find('location').text
+                                publication_code=pubmeta.get("id"),
+                                title=pubmeta.find("title").text,
+                                location=pubmeta.find("location").text,
                             )
                             publication.save()
                             issue = Issue(
-                                issue_code = issuemeta.get('id'),
-                                issue_date = issuemeta.find('date').text,
-                                input_sub_path = processmeta.find('input_sub_path').text,
-                                publication = publication
+                                issue_code=issuemeta.get("id"),
+                                issue_date=issuemeta.find("date").text,
+                                input_sub_path=processmeta.find("input_sub_path").text,
+                                publication=publication,
                             )
                             issue.save()
                             data_provider = DataProvider(
-                                name = data_provider_name,
-                                collection = 'newspapers',
-                                source_note = pubmeta.find('source').text,
+                                name=data_provider_name,
+                                collection="newspapers",
+                                source_note=pubmeta.find("source").text,
                             )
                             data_provider.save()
                             digitisation = Digitisation(
-                                xml_flavour = processmeta.find('xml_flavour').text,
-                                software = processmeta.find('software').text,
-                                mets_namespace = processmeta.find('mets_namespace').text,
-                                alto_namespace = processmeta.find('alto_namespace').text
+                                xml_flavour=processmeta.find("xml_flavour").text,
+                                software=processmeta.find("software").text,
+                                mets_namespace=processmeta.find("mets_namespace").text,
+                                alto_namespace=processmeta.find("alto_namespace").text,
                             )
                             digitisation.save()
                             ingest = Ingest(
-                                lwm_tool_name = lwm_tool.find('name').text,
-                                lwm_tool_version = lwm_tool.find('version').text,
-                                lwm_tool_source = lwm_tool.find('source').text
+                                lwm_tool_name=lwm_tool.find("name").text,
+                                lwm_tool_version=lwm_tool.find("version").text,
+                                lwm_tool_source=lwm_tool.find("source").text,
                             )
                             ingest.save()
-                            item=Item(item_code=itemmeta.get('id'), 
-                                        title=itemmeta.find('title').text, 
-                                        item_type=itemmeta.find('item_type').text,
-                                        word_count=itemmeta.find('word_count').text,
-                                        ocr_quality_mean=itemmeta.find('ocr_quality_mean').text,
-                                        ocr_quality_sd=itemmeta.find('ocr_quality_sd').text,
-                                        input_filename=itemmeta.find('plain_text_file').text,
-                                        issue=issue,
-                                        data_provider=data_provider,
-                                        digitisation=digitisation,
-                                        ingest=ingest
-                                        )  
+                            item = Item(
+                                item_code=itemmeta.get("id"),
+                                title=itemmeta.find("title").text,
+                                item_type=itemmeta.find("item_type").text,
+                                word_count=itemmeta.find("word_count").text,
+                                ocr_quality_mean=itemmeta.find("ocr_quality_mean").text,
+                                ocr_quality_sd=itemmeta.find("ocr_quality_sd").text,
+                                input_filename=itemmeta.find("plain_text_file").text,
+                                issue=issue,
+                                data_provider=data_provider,
+                                digitisation=digitisation,
+                                ingest=ingest,
+                            )
                             item.save()
