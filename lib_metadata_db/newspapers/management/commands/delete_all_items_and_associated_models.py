01993f82f5d32357e206a0f0ca3021649fe98325
ChristinaLast <christina.last@outlook.com>
2022-05-12 19:15:32 +0100

    :rotating_light: linting files

--- lib_metadata_db/newspapers/management/commands/delete_all_items_and_associated_models.py
+++ lib_metadata_db/newspapers/management/commands/delete_all_items_and_associated_models.py
@@ -13,16 +13,18 @@ class Command(BaseCommand):
     help = "Delete all model objects associated with the Items models and their foreign keys"
 
     def handle(self, *args, **options):
-        answer = input("Do you want to remove all data associated with the newspapers models? This is a destructive option and should only be done on a production database instance with the approval of the Administrator. Continue? ")
-        if answer.lower() in ["y","yes"]:
+        answer = input(
+            "Do you want to remove all data associated with the newspapers models? This is a destructive option and should only be done on a production database instance with the approval of the Administrator. Continue? "
+        )
+        if answer.lower() in ["y", "yes"]:
             Item.objects.all().delete()
             Digitisation.objects.all().delete()
             Ingest.objects.all().delete()
             Issue.objects.all().delete()
             Publication.objects.all().delete()
             DataProvider.objects.all().delete()
-             # Do other stuff
+            # Do other stuff
         else:
-             # Handle "wrong" input
-             print("Exiting deletion.")
-             pass
+            # Handle "wrong" input
+            print("Exiting deletion.")
+            pass
