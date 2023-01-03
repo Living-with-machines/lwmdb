0ab1d8b3fd580972b2b7a3463a0fcdd25a4ff8b5
Dr Griffith Rees <griff.rees@gmail.com>
2023-01-03 14:03:42 +0000

    Add gazetteer and newspapers to django admin config.

--- metadata/newspapers/admin.py
+++ metadata/newspapers/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import DataProvider, Digitisation, Ingest, Newspaper, Issue, Item
+
 # Register your models here.
+admin.site.register(DataProvider)
+admin.site.register(Digitisation)
+admin.site.register(Ingest)
+admin.site.register(Newspaper)
+admin.site.register(Issue)
+admin.site.register(Item)
