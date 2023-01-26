399735032cd0e08056ad3c5059702efe0d0a150e
Dr Griffith Rees <griff.rees@gmail.com>
2023-01-26 15:22:53 +0000

    Fix gazeteer spatial test and fix python version in GitHub CI.

--- gazetteer/tests.py
+++ gazetteer/tests.py
@@ -11,16 +11,34 @@ def manc_point() -> Point:
     Note:
         * Point.x is usually latitude and Point.y longitude.
     """
-    return Point(53.4808, 2.2426)
+    return Point(53.4808, -2.2426)
 
 
-def test_create_place(manc_point) -> None:
-    """Test creating a Place with a coordinates component without saving."""
-    test_place = Place(
-        wikidata_id="an-id-for-manchester",
-        label="Manchester",
-        latitude=manc_point.x,
-        longitude=manc_point.y,
-        coordinates=manc_point,
-    )
-    assert test_place.coordinates == manc_point
+@pytest.fixture
+def liverpool_point() -> Point:
+    """Return example centre of Manchester coordinate Point."""
+    return Point(53.400, -2.983333)
+
+
+@pytest.mark.django_db
+class TestGeoSpatial:
+    def test_create_place_and_distance(self, manc_point, liverpool_point) -> None:
+        """Test creating a Place with a coordinates component and calc distance.
+
+        Note:
+            * Curvature not included in distance between points see:
+              https://docs.djangoproject.com/en/4.2/ref/contrib/gis/geos/#django.contrib.gis.geos.GEOSGeometry.distance
+        """
+        manc_to_liverpool_2d_dist: float = 0.745126846442269
+        test_manc = Place(
+            wikidata_id="an-id-for-manchester",
+            label="Manchester",
+            latitude=manc_point.x,
+            longitude=manc_point.y,
+            coordinates=manc_point,
+        )
+        assert test_manc.coordinates == manc_point
+        test_manc.save()
+        assert (
+            test_manc.coordinates.distance(liverpool_point) == manc_to_liverpool_2d_dist
+        )
