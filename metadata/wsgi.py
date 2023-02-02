ca209d8bc46b84f6ae6e2ea600c5e919d9ac418b
Dr Griffith Rees <griff.rees@gmail.com>
2023-02-02 13:25:59 +0000

    Add GeometryCollection `geos` fields  to spatial gazeteer models.

--- metadata/wsgi.py
+++ metadata/wsgi.py
@@ -1,7 +1,7 @@
 """WSGI config for metadata project.
 
 It exposes the WSGI callable as a module-level variable named
-``application``.
+`application`.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
