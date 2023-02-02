7aa0b9be1273e2b6b8c934be78db30cf42a204d9
Dr Griffith Rees <griff.rees@gmail.com>
2023-02-02 13:25:59 +0000

    Add GeometryCollection `geos` fields  to spatial gazeteer models.

--- gazetteer/tests.py
+++ gazetteer/tests.py
@@ -1,12 +1,12 @@
 import pytest
-from django.contrib.gis.geos import Point
+from django.contrib.gis.geos import GeometryCollection, Point
 
 from .models import Place
 
 
 @pytest.fixture
 def manc_point() -> Point:
-    """Return example centre of Manchester coordinate Point.
+    """Return example centre of Manchester coordinate `Point`.
 
     Note:
         * Point.x is usually latitude and Point.y longitude.
@@ -16,7 +16,7 @@ def manc_point() -> Point:
 
 @pytest.fixture
 def liverpool_point() -> Point:
-    """Return example centre of Manchester coordinate Point."""
+    """Return example centre of Manchester coordinate `Point`."""
     return Point(53.400, -2.983333)
 
 
@@ -30,6 +30,7 @@ class TestGeoSpatial:
         Note:
             * Curvature not included in distance between points see:
               https://docs.djangoproject.com/en/4.2/ref/contrib/gis/geos/#django.contrib.gis.geos.GEOSGeometry.distance
+            * Curvature *should* be addressed when within a GeometryCollection
         """
         manc_to_liverpool_2d_dist: float = 0.745126846442269
         test_manc = Place(
@@ -37,10 +38,10 @@ class TestGeoSpatial:
             label="Manchester",
             latitude=manc_point.x,
             longitude=manc_point.y,
-            coordinates=manc_point,
+            geom=GeometryCollection(manc_point),
         )
-        assert test_manc.coordinates == manc_point
+        assert test_manc.geom.within(manc_point)
+        assert Place.objects.count() == 0
         test_manc.save()
-        assert (
-            test_manc.coordinates.distance(liverpool_point) == manc_to_liverpool_2d_dist
-        )
+        assert Place.objects.count() == 1
+        assert test_manc.geom.distance(liverpool_point) == manc_to_liverpool_2d_dist
