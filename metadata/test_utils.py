9c134c2b27f1ab4d26c8fb68f6a6a0ff306c50dc
Dr Griffith Rees <griff.rees@gmail.com>
2023-04-24 09:43:54 +0100

    feat: add production.py config with bool support

---
+++ metadata/test_utils.py
@@ -0,0 +1,20 @@
+import pytest
+
+from .utils import VALID_FALSE_STRS, VALID_TRUE_STRS, str_to_bool
+
+
+@pytest.mark.parametrize("val", VALID_TRUE_STRS)
+def test_str_to_bool_true(val):
+    assert str_to_bool(val) == True
+    assert str_to_bool(val.upper()) == True
+
+
+@pytest.mark.parametrize("val", VALID_FALSE_STRS)
+def test_str_to_bool_false(val):
+    assert str_to_bool(val) == False
+    assert str_to_bool(val.upper()) == False
+
+
+def test_str_to_bool_true_invalid(caplog):
+    with pytest.raises(ValueError):
+        str_to_bool("Truue")
