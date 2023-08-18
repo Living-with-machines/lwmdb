c298557355f03dbd067f7472007a35184017084b
Dr Griffith Rees <griff.rees@gmail.com>
2023-08-18 04:42:23 +0100

    feat(test): add fixtures and docs to facilitate adding `Fulltext`

--- newspapers/tests.py
+++ newspapers/tests.py
@@ -29,7 +29,7 @@ MODULE_LOG_PREFIX: Final[str] = "DEBUG:newspapers.models:"
 class ItemTestCase(TestCase):
     """Test creating and querying Item, Issue and Newpaper instances."""
 
-    def setUp(self):
+    def setUp(self) -> None:
         newspaper = Newspaper.objects.create(
             publication_code="0003040",
             title="The Birkenhead News and Wirral General Advertiser",
@@ -54,7 +54,7 @@ class ItemTestCase(TestCase):
             data_provider=data_provider,
         )
 
-    def test_item_parameters(self):
+    def test_item_parameters(self) -> None:
         item = Item.objects.get(item_code="0003040-18940905-art0030")
 
         self.assertEqual(item.title, TEST_ITEM_TITLE)
@@ -77,7 +77,7 @@ class ItemTestCase(TestCase):
         assert item.title_word_count == 5
 
     @patchfs
-    def test_is_downloaded(self, fs):
+    def test_is_downloaded(self, fs) -> None:
         item = Item.objects.get(item_code="0003040-18940905-art0030")
 
         self.assertFalse(item.is_downloaded())
@@ -97,7 +97,7 @@ class ItemTestCase(TestCase):
         # TODO #24: testing.
         # self.assertEqual(item.fulltext[-57:], last_57_chars)
 
-    def test_sync_title_length(self):
+    def test_sync_title_length(self) -> None:
         """Test managing title length."""
         title_extension: str = " LINE"
         test_item = Item.objects.get(item_code=TEST_ITEM_CODE)
@@ -126,7 +126,7 @@ class ItemTestCase(TestCase):
         assert logs.output == correct_logs
         assert not test_item.title_truncated
 
-    def test_sync_title_length_too_long(self):
+    def test_sync_title_length_too_long(self) -> None:
         """Test managing title length beyond max characters."""
         if not Item.MAX_TITLE_CHAR_COUNT:
             Item.MAX_TITLE_CHAR_COUNT = settings.DEFAULT_MAX_NEWSPAPER_TITLE_CHAR_COUNT
@@ -167,7 +167,7 @@ class ItemTestCase(TestCase):
             test_item.title, MAX_PRINT_SELF_STR_LENGTH
         )
 
-    def test_sync_title_length_max_title_none(self):
+    def test_sync_title_length_max_title_none(self) -> None:
         """Test unbound title length `MAX_TITLE_CHAR_COUNT = None`."""
         title_extension: str = " LINE"
         Item.MAX_TITLE_CHAR_COUNT = None
