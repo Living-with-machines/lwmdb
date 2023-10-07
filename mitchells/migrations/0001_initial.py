83d2b0639211f938489cb9a01a536145f54b57e2
Dr Griffith Rees <griff.rees@gmail.com>
2023-10-07 11:26:22 +0100

    fix: clean up `Mitchells` migration

--- mitchells/migrations/0001_initial.py
+++ mitchells/migrations/0001_initial.py
@@ -5,10 +5,6 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-    replaces = [
-        ("mitchells", "0001_initial"),
-        ("mitchells", "0002_alter_entry_options"),
-    ]
 
     initial = True
 
